d785fdd59dd629ed20b547c2c36e58b46ab6fb35
Vic Wang <723277363@qq.com>
2024-02-28 18:26:56 +0800

    删除OPENAPI链接

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -88,14 +88,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
         width: '331px',
       },
     ],
-    links: isDev
-      ? [
-        <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
-          <LinkOutlined />
-          <span>OpenAPI 文档</span>
-        </Link>,
-      ]
-      : [],
+
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
